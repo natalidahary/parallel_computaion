cdcbaaf5f5b78ad9d73819737575035d6c10ad91
linuxu <linuxu@ParallelC23-7.academic.management.afeka.local>
2023-08-23 16:58:06 +0300

    main and input

--- main_parallel.c
+++ main_parallel.c
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
     if (rank == 0)
     {
        readInputFromFile(input_file, &N, &K, &D, &tCount, &points);
+       calculateTValues(tCount, &tValues);
     }
     //Broadcast the input data (N, K, D, tCount, tValues, and points) to all processes using MPI_Bcast
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -38,7 +39,7 @@ int main(int argc, char *argv[])
     MPI_Bcast(&D, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(&tCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    calculateTValues(tCount, &tValues);
+    
     
     // Define the custom MPI datatype MPI_POINT to represent the Point structure.
     MPI_Datatype MPI_POINT;
